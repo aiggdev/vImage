c404e4ce88d7f394637ebea4f773d29535e74a3e
aiggdev <dev@aigg.dev>
2021-09-02 10:27:47 -0400

    fixed main.js, now valid

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-
+/* eslint-disable linebreak-style */
 // @ts-ignore
-import JwPagination from 'jw-vue-pagination'
-Vue.component('jw-pagination', JwPagination)
 
-Vue.config.productionTip = false
+import Vue from 'vue';
+import JwPagination from 'jw-vue-pagination';
+import App from './App.vue';
+import router from './router';
+
+Vue.component('jw-pagination', JwPagination);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
